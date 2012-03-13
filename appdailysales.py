db9628a85cb84158b3181e1ef8017a4997bdbc73
Matt Warren <mwarren@umdev13.(none)>
2012-03-13 13:27:42 -0600

    Adding scraping for daily iAd reports.

--- appdailysales.py
+++ appdailysales.py
@@ -18,7 +18,7 @@
 #
 # The iTunes Connect web site has dynamic urls and form field
 # names.  In other words, these values change from session to
-# session.  So to get to the download file we must navigate  
+# session.  So to get to the download file we must navigate
 # the site and webscrape the pages.  Joy, joy.
 #
 #
@@ -31,6 +31,7 @@
 #   Daniel Dickison
 #   Mike Kasprzak
 #   Shintaro TAKEMURA
+#   Matt Warren
 #
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -39,10 +40,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -65,6 +66,7 @@ outputFormat = None
 overWriteFiles = True
 proxy = ''
 debug = False
+publisherId = None
 # ----------------------------------------------------
 
 
@@ -78,12 +80,11 @@ import sys
 import os
 import gzip
 import StringIO
-import traceback
 import getpass
 
 
 class ITCException(Exception):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
     def __str__(self):
         return repr(self.value);
@@ -102,6 +103,7 @@ class ITCException(Exception):
 #   outputFormat
 #   overWriteFiles
 #   debug
+#   publisherId
 # Note that the class attributes will default to the global
 # variable value equivalent.
 class ReportOptions:
@@ -128,6 +130,8 @@ class ReportOptions:
             return proxy
         elif attrname == 'debug':
             return debug
+        elif attrname == 'publisherId':
+            return publisherId
         else:
             raise AttributeError, attrname
 
@@ -138,6 +142,7 @@ Options and arguments:
 -h     : print this help message and exit (also --help)
 -a uid : your apple id (also --appleId)
 -p pwd : your password (also --password)
+-b pubid : publisher ID used for iAd
 -P     : read the password from stdin (also --passwordStdin)
 -o dir : directory where download file is stored, default is the current working directory (also --outputDirectory)
 -v     : verbose output, default is off (also --verbose)
@@ -162,10 +167,11 @@ def processCmdArgs():
     global overWriteFiles
     global proxy
     global debug
+    global publisherId
 
     # Check for command line options. The command line options
     # override the globals set above if present.
-    try: 
+    try:
         opts, args = getopt.getopt(sys.argv[1:], 'ha:p:Po:uvd:D:f:n', ['help', 'appleId=', 'password=', 'passwordStdin', 'outputDirectory=', 'unzip', 'verbose', 'days=', 'date=', 'format=', 'noOverWriteFiles', 'proxy=', 'debug'])
     except getopt.GetoptError, err:
         #print help information and exit
@@ -183,6 +189,8 @@ def processCmdArgs():
             password = a
         elif o in ('-P', '--passwordStdin'):
             password = getpass.getpass()
+        elif o in ('-b', '--publisherId'):
+            publisherId = a
         elif o in ('-o', '--outputDirectory'):
             outputDirectory = a
         elif o in ('-u', '--unzip'):
@@ -225,7 +233,7 @@ class MyCookieJar(cookielib.CookieJar):
 def showCookies(cj):
     for index, cookie in enumerate(cj):
         print index, ' : ', cookie
-    
+
 
 def readHtml(opener, url, data=None, options=None):
     request = urllib2.Request(url, data)
@@ -253,7 +261,7 @@ def downloadFile(options):
     if options.proxy:                  # proxy support
         handlers.append(urllib2.ProxyHandler({"https": options.proxy}))      # proxy support
 
-    cj = MyCookieJar();
+    cj = MyCookieJar()
     cj.set_policy(cookielib.DefaultCookiePolicy(rfc2965=True))
     cjhdr = urllib2.HTTPCookieProcessor(cj)
     handlers.append(cjhdr)             # proxy support
@@ -270,11 +278,11 @@ def downloadFile(options):
     urlActionLogin = urlITCBase % match.group(1)
 
 
-    # Login to iTunes Connect web site and go to the sales 
-    # report page, get the form action url and form fields.  
-    # Note the sales report page will actually load a blank 
-    # page that redirects to the static URL. Best guess here 
-    # is that the server is setting some session variables 
+    # Login to iTunes Connect web site and go to the sales
+    # report page, get the form action url and form fields.
+    # Note the sales report page will actually load a blank
+    # page that redirects to the static URL. Best guess here
+    # is that the server is setting some session variables
     # or something.
     webFormLoginData = urllib.urlencode({'theAccountName':options.appleId, 'theAccountPW':options.password, '1.Continue':'0'})
     html = readHtml(opener, urlActionLogin, webFormLoginData, options=options)
@@ -284,7 +292,7 @@ def downloadFile(options):
     # Find the Sales and Trends URL.
     if options.verbose == True:
         print 'Accessing Sales and Trends reporting web site.'
-    
+
     # Sometimes the vendor default page does not load right away.
     # This causes the script to fail, so as a work around, the
     # script will attempt to load the page 3 times before abend.
@@ -316,7 +324,7 @@ def downloadFile(options):
                 else:
                     raise ITCException, errMessage
 
-    # This may seem confusing because we just accessed the vendor default page in the 
+    # This may seem confusing because we just accessed the vendor default page in the
     # code above. However, the vendor default page as a piece of javascript that runs
     # once the page is loaded in the browser. The javascript does a resubmit. My guess
     # is this action is needed to set the default vendor on the server-side. Regardless
@@ -420,7 +428,7 @@ def downloadFile(options):
     for downloadReportDate in reportDates:
         # Set the date within the web page.
         dateString = downloadReportDate.strftime('%m/%d/%Y')
-        
+
         if dateString in dateListAvailableDays:
             # If told not to overwrite files, check before downloading
             if (options.overWriteFiles == False):
@@ -437,7 +445,7 @@ def downloadFile(options):
 
             if options.verbose == True:
                 print 'Downloading report for: ', dateString
-            webFormSalesReportData = urllib.urlencode({'AJAXREQUEST':ajaxName, 'theForm':'theForm', 'theForm:xyz':'notnormal', 'theForm:vendorType':'Y', 'theForm:datePickerSourceSelectElementSales':dateString, 'theForm:datePickerSourceSelectElementSales':dateString, 'theForm:weekPickerSourceSelectElement':dateListAvailableWeeks[0], 'javax.faces.ViewState':viewState, selectName:selectName})
+            webFormSalesReportData = urllib.urlencode({'AJAXREQUEST':ajaxName, 'theForm':'theForm', 'theForm:xyz':'notnormal', 'theForm:vendorType':'Y', 'theForm:datePickerSourceSelectElementSales':dateString, 'theForm:weekPickerSourceSelectElement':dateListAvailableWeeks[0], 'javax.faces.ViewState':viewState, selectName:selectName})
             html = readHtml(opener, urlSalesReport, webFormSalesReportData)
             match = re.findall('"javax.faces.ViewState" value="(.*?)"', html)
             viewState = match[0]
@@ -449,8 +457,8 @@ def downloadFile(options):
             try:
                 if options.debug == True:
                     print urlHandle.info()
-                    
-                # Check for the content-disposition. If present then we know we have a 
+
+                # Check for the content-disposition. If present then we know we have a
                 # file to download. If not present then an AttributeError exception is
                 # thrown and we assume the file is not available for download.
                 filename = urlHandle.info().getheader('content-disposition').split('=')[1]
@@ -485,9 +493,9 @@ def downloadFile(options):
                 if options.verbose == True:
                     print 'Saving download file:', filename
 
-                downloadFile = open(filename, 'w')
-                downloadFile.write(filebuffer)
-                downloadFile.close()
+                download_file = open(filename, 'w')
+                download_file.write(filebuffer)
+                download_file.close()
 
                 filenames.append( filename )
             except AttributeError:
@@ -510,10 +518,217 @@ def downloadFile(options):
     return filenames
 
 
+
+
+def downloadIAdFile(options):
+    if options.verbose == True:
+        print '-- begin script --'
+
+    if (options.outputDirectory != '' and not os.path.exists(options.outputDirectory)):
+        os.makedirs(options.outputDirectory)
+
+    urlITCBase = 'https://itunesconnect.apple.com%s'
+
+    handlers = []                      # proxy support
+    if options.proxy:                  # proxy support
+        handlers.append(urllib2.ProxyHandler({"https": options.proxy}))      # proxy support
+
+    cj = MyCookieJar()
+    cj.set_policy(cookielib.DefaultCookiePolicy(rfc2965=True))
+    cjhdr = urllib2.HTTPCookieProcessor(cj)
+    handlers.append(cjhdr)             # proxy support
+    opener = urllib2.build_opener(*handlers)        # proxy support
+
+    if options.verbose == True:
+        print 'Signing into iTunes Connect web site.'
+
+    # Go to the iTunes Connect website and retrieve the
+    # form action for logging into the site.
+    urlWebsite = urlITCBase % '/WebObjects/iTunesConnect.woa'
+    html = readHtml(opener, urlWebsite, options=options)
+    match = re.search('" action="(.*)"', html)
+    urlActionLogin = urlITCBase % match.group(1)
+
+
+    # Login to iTunes Connect web site and go to the sales
+    # report page, get the form action url and form fields.
+    # Note the sales report page will actually load a blank
+    # page that redirects to the static URL. Best guess here
+    # is that the server is setting some session variables
+    # or something.
+    webFormLoginData = urllib.urlencode({'theAccountName':options.appleId, 'theAccountPW':options.password, '1.Continue':'0'})
+    html = readHtml(opener, urlActionLogin, webFormLoginData, options=options)
+    if (html.find('Your Apple ID or password was entered incorrectly.') != -1):
+        raise ITCException, 'User or password incorrect.'
+
+
+    # Sometimes the vendor default page does not load right away.
+    # This causes the script to fail, so as a work around, the
+    # script will attempt to load the page 3 times before abend.
+    vendorDefaultPageAttempts = 3
+    while vendorDefaultPageAttempts > 0:
+        vendorDefaultPageAttempts = vendorDefaultPageAttempts - 1
+        urlSalesAndTrends = 'https://reportingitc.apple.com/'
+        html = readHtml(opener, urlSalesAndTrends, options=options)
+
+        # We're at the vendor default page. Might need additional work if your account
+        # has more than one vendor.
+        try:
+            match = re.findall('"javax.faces.ViewState" value="(.*?)"', html)
+            viewState = match[0]
+            match = re.findall('script id="defaultVendorPage:(.*?)"', html)
+            defaultVendorPage = match[0]
+            ajaxName = re.sub('_2', '_0', defaultVendorPage)
+            if options.debug == True:
+                print 'viewState: ', viewState
+                print 'defaultVendorPage: ', defaultVendorPage
+                print 'ajaxName: ', ajaxName
+            vendorDefaultPageAttempts = 0 # exit loop
+        except:
+            if vendorDefaultPageAttempts < 1:
+                errMessage = 'Unable to load default vendor page.'
+                if options.verbose == True:
+                    print errMessage
+                    raise
+                else:
+                    raise ITCException, errMessage
+
+    # This may seem confusing because we just accessed the vendor default page in the
+    # code above. However, the vendor default page as a piece of javascript that runs
+    # once the page is loaded in the browser. The javascript does a resubmit. My guess
+    # is this action is needed to set the default vendor on the server-side. Regardless
+    # we must call the page again but no parsing of the HTML is needed this time around.
+    urlDefaultVendorPage = 'https://reportingitc.apple.com/vendor_default.faces'
+    webFormSalesReportData = urllib.urlencode({'AJAXREQUEST':ajaxName, 'javax.faces.ViewState':viewState, 'defaultVendorPage':defaultVendorPage, 'defaultVendorPage:'+defaultVendorPage:'defaultVendorPage:'+defaultVendorPage})
+    html = readHtml(opener, urlDefaultVendorPage, webFormSalesReportData, options=options)
+
+    # Check for notification messages.
+    urlDashboard = 'https://reportingitc.apple.com/subdashboard.faces'
+    html = readHtml(opener, urlDashboard, options=options)
+    try:
+        # Note the (?s) is an inline re.DOTALL, makes . match new lines.
+        match = re.findall('(?s)<div class="notification">(.*?)</span>', html)
+        notificationDiv = match[0]
+        match = re.findall('(?s)<td>(.*?)</td>', notificationDiv)
+        notificationMessage = match[0]
+        if options.verbose == True:
+            print notificationMessage
+    except:
+        pass # Do nothing. We're just checking for notifications.
+
+    # Access the sales report page.
+    if options.verbose == True:
+        print 'Accessing sales report web page.'
+    urlSalesReport = 'https://iad.apple.com/itcportal/'
+    html = readHtml(opener, urlSalesReport, options=options)
+    #print html
+
+
+    if not options.publisherId:
+        #Find the publisher ID
+        params = {'requestId':'GUEST_1331628761939_0__1331628761950__app_homepage',} #the two big numbers here are milliseconds - presumably to measure page performance
+        headers = {'Content-type': 'text/x-gwt-rpc; charset=utf-8'}
+        urlDefaultiAdCsvPage = 'https://iad.apple.com/itcportal/service/startup'
+        webFormSalesReportData = urllib.urlencode(params)
+        body = '5|0|4|https://iad.apple.com/itcportal/itcportal/|4CC9623D5B1BC24D1D88612368C8A35F|com.qwapi.portal.itc.client.rpc.IITCStartupService|getStartupData|1|2|3|4|0|'
+        request = urllib2.Request(urlDefaultiAdCsvPage + "?" + webFormSalesReportData, body, headers)
+        urlHandle = opener.open(request)
+        startupData = urlHandle.read()
+        endpos = startupData.find('com.qwapi.portal.client.rpc.dto.UserDTO')
+        endpos = startupData.find('"', endpos-4)
+        startpos = startupData.rfind('"', endpos-20, endpos-4)
+        if options.verbose:
+            print "found PublisherID:", startupData[startpos+1:endpos]
+        options.publisherID = startupData[startpos+1:endpos]
+        if not int(options.publisherID):
+            raise ITCException("Failed to get publisher ID")
+
+    reportDates = []
+    if options.dateToDownload == None:
+        for i in range(int(options.daysToDownload)):
+            today = datetime.date.today() - datetime.timedelta(i + 1)
+            reportDates.append( today )
+    else:
+        reportDates = [datetime.datetime.strptime(options.dateToDownload, '%m/%d/%Y').date()]
+
+    unavailableCount = 0
+    filenames = []
+    for downloadReportDate in reportDates:
+        # If told not to overwrite files, check before downloading
+        if (options.overWriteFiles == False):
+            # Only if custom formatting was enabled, we can check if file exists before downloading
+            if (options.outputFormat):
+                filename = downloadReportDate.strftime(options.outputFormat)
+                filename = os.path.join(options.outputDirectory, filename)
+                if (os.path.exists(filename)):
+                    if options.verbose == True:
+                        print 'Report file', filename, 'exists, skipping.'
+                    continue
+
+        # Set the date within the web page.
+        dateString = downloadReportDate.strftime('%m/%d/%Y')
+
+        params = {'pageName': 'app_homepage',
+            'dashboardType':'publisher',
+            'publisherId':options.publisherID,
+            'dateRange':'customDateRange',
+            'searchTerms':'Search Apps',
+            'adminFlag':'false',
+            'fromDate':dateString,
+            'toDate':dateString,
+            'dataType':'byName'
+        }
+        urlDefaultiAdCsvPage = 'https://iad.apple.com/itcportal/generatecsv'
+        webFormSalesReportData = urllib.urlencode(params)
+        request = urllib2.Request(urlDefaultiAdCsvPage + '?' + webFormSalesReportData)
+        urlHandle = opener.open(request)
+
+
+        try:
+            if options.debug == True:
+                print urlHandle.info()
+
+            # Check for the content-disposition. If present then we know we have a
+            # file to download. If not present then an AttributeError exception is
+            # thrown and we assume the file is not available for download.
+            filename = urlHandle.info().getheader('content-disposition').split('=')[1]
+            # Check for an override of the file name. If found then change the file
+            # name to match the outputFormat.
+            if (options.outputFormat):
+                filename = downloadReportDate.strftime(options.outputFormat)
+
+            filebuffer = urlHandle.read()
+            urlHandle.close()
+
+            filename = os.path.join(options.outputDirectory, filename)
+
+            if options.verbose == True:
+                print 'Saving download file:', filename
+
+            download_file = open(filename, 'w')
+            download_file.write(filebuffer)
+            download_file.close()
+
+            filenames.append( filename )
+        except AttributeError:
+            print '%s report is not available - try again later.' % dateString
+            unavailableCount += 1
+
+    if unavailableCount > 0:
+        raise ITCException, '%i report(s) not available - try again later' % unavailableCount
+
+    if options.debug == True:
+        os.remove(os.path.join(options.outputDirectory, "temp.html"))
+    if options.verbose == True:
+        print '-- end of script --'
+
+    return filenames
+
+
 def main():
     if processCmdArgs() > 0:    # Will exit if usgae requested or invalid argument found.
-      return 2
-      
+        return 2
+
     # Set report options.
     options = ReportOptions()
     options.appleId = appleId
@@ -527,7 +742,7 @@ def main():
     options.overWriteFiles = overWriteFiles
     options.proxy = proxy
     options.debug = debug
-    
+
     # Download the file.
     try:
         downloadFile(options)
@@ -537,4 +752,4 @@ def main():
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
